b0ae5e7f614ca34592a4d9aacdda3f08772eb420
tuba balkan <tubabalkan165@gmail.com>
2024-05-21 22:58:08 +0300

    Add project files.

---
+++ BlogForest.DataAccessLayer/Context/BlogContext.cs
@@ -0,0 +1,23 @@
+﻿using BlogForest.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogForest.DataAccessLayer.Context
+{
+    public class BlogContext:IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-E4NV9FV;initial Catalog=BlogDbForest;integrated Security=true");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
